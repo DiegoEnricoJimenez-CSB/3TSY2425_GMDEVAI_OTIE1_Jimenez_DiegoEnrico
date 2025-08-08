8ed39575af22d0ec7d667a0473601efaa347da10
DiegoEnricoJimenez-CSB <163430925+DiegoEnricoJimenez-CSB@users.noreply.github.com>
2025-08-08 22:59:21 +0800

    pushed Milestone 5

---
+++ Milestone 5 Complex Behaviors/Assets/Agent1.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Agent1 : AIControl
+{
+
+    void Update()
+    {
+        if (isNearTarget())
+        {
+            Pursue();
+        }
+        else
+        {
+            Wander();
+        }
+    }
+}
---
+++ Milestone 5 Complex Behaviors/Assets/Agent2.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Agent2 : AIControl
+{
+    // Update is called once per frame
+    void Update()
+    {
+        if (isNearTarget() && canSeeTarget())
+        {
+            CleverHide();
+        }
+        else
+        {
+            Wander();
+        }
+    }
+}
