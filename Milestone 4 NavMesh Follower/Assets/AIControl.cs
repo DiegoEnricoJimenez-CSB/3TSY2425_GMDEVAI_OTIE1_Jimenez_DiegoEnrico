e3ad6333bb71d0c5e597a543b12bfb2359effc43
DiegoEnricoJimenez-CSB <163430925+DiegoEnricoJimenez-CSB@users.noreply.github.com>
2025-08-08 22:53:16 +0800

    pushed Milestone 4

---
+++ Milestone 4 NavMesh Follower/Assets/AIControl.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class AIControl : MonoBehaviour
+{
+    public UnityEngine.AI.NavMeshAgent agent;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        this.agent = this.GetComponent<UnityEngine.AI.NavMeshAgent>();
+    }
+
+}
---
+++ Milestone 4 NavMesh Follower/Assets/AgentManager.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AgentManager : MonoBehaviour
+{
+    GameObject[] agents;
+    public GameObject targetPlayer;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        agents = GameObject.FindGameObjectsWithTag("AI");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            RaycastHit hit;
+
+            if (Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out hit, 1000))
+            {
+                foreach (GameObject ai in agents)
+                {
+                    ai.GetComponent<AIControl>().agent.SetDestination(hit.point);
+                }
+            }
+
+        }
+        else
+        {
+            foreach (GameObject ai in agents)
+            {
+                ai.GetComponent<AIControl>().agent.SetDestination(targetPlayer.transform.position);
+                if (ai.GetComponent<AIControl>().agent.remainingDistance < 5)
+                    ai.GetComponent<AIControl>().agent.isStopped = true;
+                else
+                    ai.GetComponent<AIControl>().agent.isStopped = false;
+            }
+        }
+    }
+}
